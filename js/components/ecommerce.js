70b39377345d065f662ba5d090b999a8f7f1e7f5
Razvan Matei <razvanmateid@gmail.com>
2016-04-14 10:47:47 +0300

    fix module exports

--- js/components/ecommerce.js
+++ js/components/ecommerce.js
@@ -3,9 +3,14 @@ import ClassNames from 'classnames';
 
 import { Block, Btn, Card, CardHeader, CardContent, CardFooter, Image, Text, Divider, Menu, List, ListItem, Icon, Input, Slider, Switch } from 'react-essence';
 
-class PatagoniaCommerce extends React.Component {
+class OnebrandEcommerce extends React.Component {
   render() {
     return (
       <Block>
         ecommerce
       </Block>
+    );
+  }
+}
+
+module.exports = OnebrandEcommerce;
